02a972c9d7aad28e5e98bbb26723a40e35474b40
Marshall Humble <marshallhumble@users.noreply.github.com>
2015-09-30 12:16:48 -0500

    Update CapitolWords.py

--- CapitolWords.py
+++ CapitolWords.py
@@ -4,7 +4,7 @@
 CapitolWords.py:
 Pull a phrase from the Capitol Words API and place the dates of US Congress in Session,
 the total count that day of the word being said, and also the tf-idf count.
-The td-idf count is erm frequency–inverse document frequency which shows the word usage
+The td-idf count is term frequency–inverse document frequency which shows the word usage
 in relation to other words used that day.
 """
 
