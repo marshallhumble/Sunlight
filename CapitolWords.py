47e558d3c50edb0954dff0344bf32d2eab8bd972
Marshall Humble <marshallhumble@users.noreply.github.com>
2015-09-29 21:45:48 -0500

    Update CapitolWords.py

--- CapitolWords.py
+++ CapitolWords.py
@@ -8,22 +8,19 @@ The td-idf count is erm frequency–inverse document frequency which shows the w
 in relation to other words used that day.
 """
 
-__author__      = "Marshall Humble"
-__copyright__   = "MIT License"
+__author__ = "Marshall Humble"
+__copyright__ = "MIT License"
 
+import requests, json, csv
 
-import requests
-import json
-import csv
-
-#Define the Phrase we want to look for
+# Define the Phrase we want to look for
 my_phrase = 'economy'
+csv_file = my_phrase + '.csv'
 
 
 def get_words(search_phrase):
-
-    #Communitcate with the API via requests
-    api_key = '<YOUR_API_KEY:https://sunlightfoundation.com/api/>'
+    # Communitcate with the API via requests
+    api_key = 'f6ab5f2e4f69444b9f2c0a44d9a5223d'
     base_url = 'http://capitolwords.org/api/1/dates.json?'
     time_series__payload = {
         'phrase': search_phrase,
@@ -32,15 +29,14 @@ def get_words(search_phrase):
         'apikey': api_key
     }
 
-    #take the response and turn it into workable JSON
+    # take the response and turn it into workable JSON
     r = requests.get(base_url, params=time_series__payload)
     json_string = r.text
-    new_file = my_phrase + '.csv'
-    f = csv.writer(open(new_file, "wt"))
     json_data = json.loads(json_string)
     results = json_data['results']
 
-    #write the data to a csv
+    # write the data to a csv
+    f = csv.writer(open(csv_file, "wt"))
     f.writerow(["date", "count", "tf-idf"])
     for x in results:
         f.writerow([x["day"],
