76ae220b9c13d552119b858893ab505b5cad2f34
Marshall Humble <marshallhumble@users.noreply.github.com>
2015-10-01 20:39:40 -0500

    PEP 8 compliance, added in argv & error checks

--- CapitolWords.py
+++ CapitolWords.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import requests
+import json
+import csv
+from os import path
+from sys import exit, argv
+
 """
 CapitolWords.py:
 Pull a phrase from the Capitol Words API and place the dates of US Congress in Session,
@@ -11,61 +17,64 @@ in relation to other words used that day.
 __author__ = "Marshall Humble"
 __copyright__ = "MIT License"
 
-import requests, json, csv
-from os import path
-from sys import exit
-
 # Define the Phrase we want to look for
-my_phrase = 'economy'
-csv_file = my_phrase + '.csv'
+
+if len(argv) == 2:
+	print("Searching for phrase", argv[1])
+	my_phrase = argv[1]
+else:
+	my_phrase = input("Please enter a search term: ")
+
+csv_file = my_phrase + '_file.csv'
 
 
-def get_words(search_phrase):
-    # Communitcate with the API via requests
-    api_key = '<YOUR API KEY>'
-    base_url = 'http://capitolwords.org/api/1/dates.json?'
-    time_series__payload = {
-        'phrase': search_phrase,
-        'percentages': 'true',
-        'granularity': 'day',
-        'apikey': api_key
-    }
+def get_capitol_words(search_phrase):
+	# Communitcate with the API via requests
+	api_key = 'f6ab5f2e4f69444b9f2c0a44d9a5223d'
+	base_url = 'http://capitolwords.org/api/1/dates.json?'
+	time_series__payload = {
+		'phrase': search_phrase,
+		'percentages': 'true',
+		'granularity': 'day',
+		'apikey': api_key
+	}
 
-    # Check to see if the file exists, if it does not get it.
-    if path.isfile(csv_file) == False:
+	# Check to see if the file exists, if it does not get it.
+	if not path.isfile(csv_file):
+		# Let's try to grab the info from the API
+		try:
+			r = requests.get(base_url, params=time_series__payload)
 
-        #Let's try to grab the info from the API
-        try:
-            r = requests.get(base_url, params=time_series__payload)
+		# Handle the various requests
+		except requests.exceptions.Timeout:
+			print("Request timeout.")
+			exit(1)
+		except requests.exceptions.TooManyRedirects:
+			print("Incorrect URL, please check")
+			exit(1)
+		except requests.exceptions.RequestException as e:
+			print(e)
+			exit(1)
+		else:
 
-        #Handle the various requests
-        except requests.exceptions.Timeout:
-            print("Request timeout.")
-        except requests.exceptions.TooManyRedirects:
-            print("Incorrect URL, please check")
-        except requests.exceptions.RequestException as e:
-            print (e)
-            exit(1)
-        else:
+			# clean up the JSON
+			json_string = r.text
+			json_data = json.loads(json_string)
+			results = json_data['results']
 
-            #clean up the JSON
-            json_string = r.text
-            json_data = json.loads(json_string)
-            results = json_data['results']
+			# write the data to a csv
+			f = csv.writer(open(csv_file, "wt"))
+			f.writerow(["date", "count", "tf-idf"])
+			for x in results:
+				f.writerow([x["day"], x["total"], x["percentage"]])
+			print(csv_file, "written, exiting")
+			exit(0)
 
-            # write the data to a csv
-            f = csv.writer(open(csv_file, "wt"))
-            f.writerow(["date", "count", "tf-idf"])
-            for x in results:
-                f.writerow([x["day"],
-                            x["total"],
-                            x["percentage"]])
-            print (csv_file, "written, exiting")
-            exit(0)
+	# Exit if the file already exists
+	elif path.isfile(csv_file):
+		print(csv_file, "exists, exiting")
+		exit(0)
 
-    #Exit if the file already exists
-    elif path.isfile(csv_file)  == True:
-        print (csv_file, "exists, exiting")
-        exit(0)
 
-get_words(my_phrase)
+if __name__ == "__main__":
+	get_capitol_words(my_phrase)
